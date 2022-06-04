93e387ec52dae8ae0f4916e93bc2b9b7e5060c92
mrm107 <mohamadireza107@gmail.com>
2022-06-04 19:37:14 +0430

    refactor: create sticky header

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -13,10 +13,38 @@ $(".hamburger-menu").click(function (e) {
             "transform": "rotateZ(0deg)",            
         });
         $(".navbar").animate({
-            // "left": "-500px",
             "top": "-1000px",
 
         });
     }
        
 });
+
+
+
+
+window.onscroll = ()=> {test()};
+let header = $(".top-header-container");
+let lastpos = 0;
+function test(){
+    let current=window.pageYOffset;
+    if (current > 64 && lastpos===0){
+        lastpos=1;
+        Stick(1);
+    }
+    if(current < 64 && lastpos===1){
+        lastpos=0;
+        Stick(0);
+    }
+}
+
+
+function Stick(e) {
+    console.log("first");
+  if (e===1) {
+      $(header).addClass("sticky");
+  } else {
+      $(header).removeClass("sticky");
+  }
+}
+
