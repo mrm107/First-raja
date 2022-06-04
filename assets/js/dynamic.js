cbed2f0172f691f151805322a6f0782d063c2ae4
mrm107 <mohamadireza107@gmail.com>
2022-06-04 16:17:20 +0430

    refactor:add hover effect and animation in card and menu

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -1,12 +1,21 @@
 $(".hamburger-menu").click(function (e) { 
     if($("#hamburger").is(':checked')){
+        $(".hamburger-menu").css({
+            "transform": "rotateZ(45deg)",            
+        });
         $(".navbar").animate({
             "left": "0",
+            "top": "64px",
         }); 
     }
     else{
+        $(".hamburger-menu").css({
+            "transform": "rotateZ(0deg)",            
+        });
         $(".navbar").animate({
-            "left": "-500px",
+            // "left": "-500px",
+            "top": "-1000px",
+
         });
     }
        
